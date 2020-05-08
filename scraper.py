b61ef36852098b7fd35c6f975df69a81920e4b92
Alexandar Rangelov <alexrangelov90@gmail.com>
2020-05-08 21:15:28 +0100

    Adding $TSLA and $AAPL

--- scraper.py
+++ scraper.py
@@ -18,14 +18,28 @@ def sp500Price():
     sp500_price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
     print(f"s&p 500 stock market price: {sp500_price}")
 
+def TeslaPrice():
+    URL = "https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch"
+    respone = requests.get(URL)
+    soup = BeautifulSoup(respone.text, 'html.parser')
+    tesla_price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+    print(f"Tesla stock market price: {tesla_price}")
 
+def ApplePrice():
+    URL = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
+    respone = requests.get(URL)
+    soup = BeautifulSoup(respone.text, 'html.parser')
+    apple_price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+    print(f"Apple stock market price: {apple_price}")
 
 
 while True:
     print (now.now().strftime("%H:%M:%S"))
-            
+
     #Calling the functions
     sp500Price()
+    TeslaPrice()
+    ApplePrice()
 
     #Repeat function calling every 60 seconds
     time.sleep(60)
