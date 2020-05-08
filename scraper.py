61950828ce6179e91c9a7d1f17ba3e0093e1e8c2
Alexandar Rangelov <alexrangelov90@gmail.com>
2020-05-08 17:01:21 +0100

    Scraping s&p 500 price

---
+++ scraper.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+#Function that scrap s&p 500 price
+def sp500Price():
+    URL = "https://finance.yahoo.com/quote/%5EGSPC?p=^GSPC"
+
+    #Parsing the page
+    respone = requests.get(URL)
+    soup = BeautifulSoup(respone.text, 'html.parser')
+
+    #Getting and printing the price
+    sp500_price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+    print(f"s&p 500 stock market price: {sp500_price}")
+
+
+
+
+while True:
+    sp500Price()
+    time.sleep(60)
